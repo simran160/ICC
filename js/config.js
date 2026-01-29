64363fcb6591bf2533181607a0f0c761c76b4442
vivek0205 <vivek.co@gmail.com>
2026-01-29 21:33:33 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,12 +1,13 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_gnVpYyTdh', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '2s726v95te7k8fl18po4mced7l', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_DGfsfknWC', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6ulnhcdqmkikrgh9he68sp9c26', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
