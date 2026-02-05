6ceb283e182e20925e7b078a35764f46d0bd92ed
simran160 <siimrann.kaurr@gmail.com>
2026-02-05 12:56:06 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,13 +1,14 @@
 window._config = {
     cognito: {
-        userPoolId: '4re046phert8hnmllijaaal5gk', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7hqkvhir76sue3fbhe259k09cf', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_eGwPN74sC', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4re046phert8hnmllijaaal5gk', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://ah93yq8y22.execute-api.us-east-1.amazonaws.com/Dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
