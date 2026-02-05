e163a1b86dbf75faab056749593729356878bb2a
simran160 <siimrann.kaurr@gmail.com>
2026-02-05 12:55:29 +0530

    Updated

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_CxwfmVV6o', // e.g. us-east-2_uXboG5pAb
+        userPoolId: '4re046phert8hnmllijaaal5gk', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '7hqkvhir76sue3fbhe259k09cf', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
@@ -8,6 +8,7 @@ window._config = {
         invokeUrl: 'https://ah93yq8y22.execute-api.us-east-1.amazonaws.com/Dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
